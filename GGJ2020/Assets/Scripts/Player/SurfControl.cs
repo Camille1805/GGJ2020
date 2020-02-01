c59e4fa4c5497034c116815061bdc98c9f895538
arramon.alexandre@gmail.com <arramon.alexandre@gmail.com>
2020-02-01 17:02:48 +0100

    fix rotation for surf, add post processing profile to scene

--- GGJ2020/Assets/Scripts/Player/SurfControl.cs
+++ GGJ2020/Assets/Scripts/Player/SurfControl.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 
 public class SurfControl : MonoBehaviour
 {
-    [SerializeField] private float thrust;
-    [SerializeField] private ForceMode forwardForceMode;
-    [SerializeField] private float rotationForce;
+    [SerializeField] private float thrust = 10.0f;
+    [SerializeField] private ForceMode forwardForceMode = ForceMode.Impulse;
+    [SerializeField] private float rotationForce = 25.0f;
     [SerializeField] private Rigidbody rigidBody;
+    [SerializeField] private bool showDebug = false;
     private InputManager inputManager;
 
     void Start()
@@ -28,15 +29,21 @@ public class SurfControl : MonoBehaviour
     {
         float forwardForce = thrust * yAxisValue;
         rigidBody.AddForce(transform.forward * forwardForce, forwardForceMode);
-        // Debug.Log("forwardForce" + forwardForce + ", directionY" + yAxisValue);
+        if (showDebug)
+        {
+            Debug.Log("forward" + transform.forward);
+        }
     }
 
     private void RotateSurf(Vector2 directions)
     {
-        Vector3 eulerAngles = transform.eulerAngles;
-        eulerAngles.y += directions.x * rotationForce;
-        Quaternion eulerRot = Quaternion.Euler(eulerAngles);
+        Vector3 localEulerAngles = transform.localEulerAngles;
+        localEulerAngles.y += directions.x * rotationForce;
+        Quaternion eulerRot = Quaternion.Euler(localEulerAngles);
         transform.rotation = Quaternion.Slerp(transform.rotation, eulerRot, Time.deltaTime * 10);
-        Debug.Log(eulerRot + " " + directions.x);
+        if (showDebug)
+        {
+            Debug.Log("eulerRot: " + eulerRot + ", directions.x: " + directions.x);
+        }
     }
 }
