7a895e895ad59c10c9bf629c3998330490358022
arramon.alexandre@gmail.com <arramon.alexandre@gmail.com>
2020-02-02 17:12:50 +0100

    final level 1

---
+++ GGJ2020/Assets/TitleScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleScript : MonoBehaviour
+{
+    public Image image;
+    // Start is called before the first frame update
+
+    IEnumerator FadeOutAfter(float time)
+    {
+        yield return new WaitForSeconds(time);
+        image.CrossFadeAlpha(0, 2, true);
+        // Code to execute after the delay
+    }
+
+    void Start()
+    {
+        StartCoroutine(FadeOutAfter(3));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
