6651e1431f64ac562f11518612afeb7624615343
Camille <hypoter@hotmail.fr>
2020-02-14 08:49:28 +0100

    commit pour pull

---
+++ GGJ2020/Assets/AudioManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    static AudioManager am=null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (am != null) {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Debug.Log("ldldkd");
+            am = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+}
